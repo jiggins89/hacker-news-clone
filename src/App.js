b7d1ac59dd4099a027f6778c397c51c1b8ac2735
John Higgins <johnhiggins@outlook.ie>
2019-07-02 15:56:56 +0100

    searchbar added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Library imports
+import React from "react";
+// Component imports
+import SearchBar from "./components/SearchBar";
+// Style imports
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBar />
+      <h1>NewsList</h1>
     </div>
   );
 }
